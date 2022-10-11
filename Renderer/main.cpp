da3589d23696c0963d008bfa22eaef26f3f1ba18
HiveYuan <623383027@qq.com>
2022-10-10 22:23:29 -0500

    setup tga class

--- Renderer/main.cpp
+++ Renderer/main.cpp
@@ -6,9 +6,14 @@
 //
 
 #include <iostream>
+#include "tgaimage.h"
 
-int main(int argc, const char * argv[]) {
-    
-    std::cout << "Hello, World!\n";
-    return 0;
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+
+
+int main(int argc, char** argv) {
+    TGAImage image(100, 100, TGAImage::RGB);
+    image.set(52, 41, red);
+    return image.write_tga_file("output.tga");
 }
