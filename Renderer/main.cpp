8061a2da674d5f02c2022d302b0cef0c5f511235
HiveYuan <623383027@qq.com>
2022-10-10 21:52:21 -0500

    init repo

---
+++ Renderer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Renderer
+//
+//  Created by Yuan Liu on 10/10/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
